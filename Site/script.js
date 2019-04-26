38f431c73db82378559023d4154ebca7c4cea814
RomainChaillou <43874648+RomainChaillou@users.noreply.github.com>
2019-04-26 09:47:05 +0200

    maj map

--- Site/script.js
+++ Site/script.js
@@ -1,24 +1,9 @@
-var map = new ol.Map({
-  target: document.getElementById('map'),
-	layers : [
-	  new ol.layer.Tile({
-		source: new ol.source.XYZ({
-			url: 'https://tile.jawg.io/d5083f83-4b58-4bca-8a5c-c4888af10610/{z}/{x}/{y}.png?access-token=7R3sFk6cRCKtNZ8SO9RwYnpZsGEUs48u4wQRlnbzJcC5M0TzX8mLIsboUE3URvq2'
-		})
-	  })
-	],
-	   view : new ol.View({
-		center: [-1.563389, 47.210917],
-		projection: 'EPSG:4326',
-		zoom: 13
-	})
-});
- new ol.layer.Vector({
+
+var vectorSource = new ol.source.Vector();
+
+var vector = new ol.layer.Vector({
    title: 'coordinates',
-   source: new ol.source.Vector({
-		url: 'data.geojson',
-		format: new ol.format.GeoJSON()	 
-   }),
+   source: vectorSource,
    style: new ol.style.Style({
      image: new ol.style.Circle({
        radius: 5,
@@ -26,3 +11,45 @@ var map = new ol.Map({
      })
    })
  })
+ var center = ol.proj.transform([-1.563389, 47.210917], 'EPSG:4326', 'EPSG:3857');
+
+ var map = new ol.Map({
+  target: document.getElementById('map'),
+	layers : [
+	  new ol.layer.Tile({
+		source: new ol.source.XYZ({
+			url: 'https://tile.jawg.io/d5083f83-4b58-4bca-8a5c-c4888af10610/{z}/{x}/{y}.png?access-token=7R3sFk6cRCKtNZ8SO9RwYnpZsGEUs48u4wQRlnbzJcC5M0TzX8mLIsboUE3URvq2'
+		})
+	  }),
+	  vector
+	],
+	   view : new ol.View({
+		center: center,
+		zoom: 13
+	})
+});
+
+function updatePoint(data) {
+      // we need to transform the geometries into the view's projection
+      var transform = ol.proj.getTransform('EPSG:4326', 'EPSG:3857');
+      // loop over the items in the response
+      data.items.forEach(function(item) {
+        // create a new feature with the item as the properties
+        var feature = new ol.Feature(item);
+        // add a url property for later ease of access
+        feature.set('url', item.media.m);
+        // create an appropriate geometry and add it to the feature
+        var coordinate = transform([parseFloat(item.longitude), parseFloat(item.latitude)]);
+        var geometry = new ol.geom.Point(coordinate);
+        feature.setGeometry(geometry);
+        // add the feature to the source
+        vectorSource.addFeature(feature);
+      });
+    }
+	
+$.ajax({
+  url: 'data.json',
+  dataType: 'json',
+  //jsonpCallback: 'jsonFlickrFeed',
+  success: updatePoint
+});
