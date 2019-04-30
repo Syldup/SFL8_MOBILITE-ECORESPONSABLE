932b1f4a8f789413e8f0b7833a2f3ee4a9b4bd98
RomainChaillou <43874648+RomainChaillou@users.noreply.github.com>
2019-04-30 15:55:24 +0200

    maj

--- Site/connexion.php
+++ Site/connexion.php
@@ -1,38 +1,13 @@
 <?php
-	// On définit les 4 variables nécessaires à la connexion MySQL :
-	$hostname = "localhost";
-	$port     = 3306;
-	$user     = "root";
-	$password = "";
-	$nom_base_donnees = "sfl8_bdd";
+  $host_name = 'db5000063389.hosting-data.io';
+  $database = 'dbs58127';
+  $user_name = 'dbu192881';
+  $password = '@SLF8_system';
+  $connect = mysql_connect($host_name, $user_name, $password, $database);
 
-
-	// VIEILLE METHODE (sans PDO)
-	// A ne plus utiliser normalement ...
-	// compatible PHP <= 5.1
-	//		$conn = mysql_connect($hostname, $user, $password) or die(mysql_error());
-
-	//		// Choix de la base sur laquelle travailler
-	//		mysql_select_db($nom_base_donnees, $conn);
-
-	//		// pour ceux qui ont l'extension mysqli_* on peut remplacer tous les mysql_* par mysqli_*
-	//
-
-
-
-	// NOUVELLE METHODE (depuis PHP 5.1 et avec PDO)
-	// depuis PHP 5.1 et a fortiori en PHP 7, on doit utiliser la classe "PDO" (PHP Data Object, un objet de connexion)
-	// connexion au serveur MySQL avec PDO (la forme plus récente, et plus sécurisée : NOUVELLE METHODE
-	try
-	{
-		$connexion = new PDO('mysql:host='.$hostname.';port='.$dbport.';dbname='.$nom_base_donnees, $user, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-		$connexion->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-	}
-	catch(Exception $e)
-	{
-		echo 'Erreur : '.$e->getMessage().'<br />';
-		echo 'N° : '.$e->getCode();
-		exit();
-	}
-
+  if (mysql_errno()) {
+    die('<p>La connexion au serveur MySQL a échoué: '.mysql_error().'</p>');
+  } else {
+    echo '<p>Connexion au serveur MySQL établie avec succès.</p >';
+  }
 ?>
---
+++ microparticule/Get_co2/Get_co2.ino
@@ -0,0 +1,54 @@
+int pin = 8;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 10000;//sampe 2s ;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+int sensorValue;
+int digitalValue;
+void setup()
+{
+
+Serial.begin(9600); // sets the serial port to 9600
+pinMode(13, OUTPUT);
+pinMode( 3, INPUT);
+pinMode(pin,INPUT);
+starttime = millis();//get the current time;
+
+}
+
+
+void loop()
+
+{
+
+sensorValue = analogRead(0); // read analog input pin 0
+
+digitalValue = digitalRead(2); 
+if(sensorValue>400)
+{
+digitalWrite(13, HIGH);
+}
+else
+digitalWrite(13, LOW);
+Serial.println(sensorValue, DEC); // prints the value read
+
+delay(10000); // wait 100ms for next reading
+
+ duration = pulseIn(pin, LOW);
+    lowpulseoccupancy = lowpulseoccupancy+duration;
+
+    if ((millis()-starttime) > sampletime_ms)//if the sampel time == 30s
+    {
+        ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+        concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+        Serial.print(lowpulseoccupancy);
+        Serial.print(",");
+        Serial.println(ratio);
+        Serial.print("concentration : ");
+        Serial.println(concentration);
+        lowpulseoccupancy = 0;
+        starttime = millis();
+    }
+}
